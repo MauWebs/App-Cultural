8995464d3537fd16f3d6e73deec89d2d9a308490
MauWebs <mauriciovidalwebs@gmail.com>
2023-11-05 23:49:28 -0300

    commit

--- virtualReality/views.py
+++ virtualReality/views.py
@@ -12,7 +12,6 @@ from virtualReality.models import VirtualReality
 
 from .serializers import VirtualRealitySerializer
 
-
 # --------------------------------------------------------------------------- #
 
 
@@ -29,26 +28,30 @@ def save_uploaded_image(uploaded_image):
 
 
 @api_view(['POST'])
-@permission_classes([IsAdminUser])
 def postVirtualReality(request):
-   
+
     if request.method == 'POST':
-       
         serializer = VirtualRealitySerializer(data=request.data, context={'request': request})
 
         if serializer.is_valid():
-           
             uploaded_image = request.FILES.get('img')
+            
             if uploaded_image:
                 image_path = save_uploaded_image(uploaded_image)
                 serializer.validated_data['img'] = image_path
-
-            serializer.save()
+            
+            VirtualReality.objects.create(
+                title=serializer.validated_data['title'],
+                description=serializer.validated_data['description'],
+                place=serializer.validated_data['place'],
+                format=serializer.validated_data['format'],
+                tag=serializer.validated_data['tag'],
+                image=serializer.validated_data['img'] 
+            )
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 # --------------------------------------------------------------------------- #
